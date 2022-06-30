0375348e37d6247aebb9f8feab0d10eb037d2d2c
S Rabi <rabi.sanaye@yahoo.com>
2022-06-30 12:40:10 +0200

    first exercise

---
+++ solution.js
@@ -0,0 +1,15 @@
+/**
+ * Create a JavaScript file solution.js and work from there.
+ * Print the phrase Hello World. Make sure to run your javascript file in node.
+ * Print another phrase This is my first js file.
+ */
+console.log("Hellow world.");
+console.log("This is my first js file.");
+/**
+ * Remove a parenthesis from the first statement and run the code. Does your statement print?
+ * Is there an error? If so, what kind of error is it?
+ */
+console.log "Hello world.";
+/**
+ * SyntaxError: Unexpected string
+ */
